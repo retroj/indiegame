a5fb2ae90c45a155616638ca8569980cd7b194f5
John Foerch <jjfoerch@earthlink.net>
2012-11-19 23:06:38 -0500

    indiegame website, initial checkin

---
+++ indiegame.scm
@@ -0,0 +1,16 @@
+
+(import chicken scheme extras)
+
+(use srfi-1
+     doctype
+     awful)
+
+(page-doctype doctype-html)
+(page-charset "UTF-8")
+(enable-sxml #t)
+(literal-script/style? #t)
+
+(define-page "/"
+  (lambda ()
+    `((h1 "Indie Game Dev Simulator: The Indie Game Development Simulator Game")))
+  title: "Indie Game Dev Simulator: The Indie Game Development Simulator Game")
